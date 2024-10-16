9031b3ce853cc611ea0b63a740a26fab9888a4f8
fifteen <qfifteen@163.com>
2024-10-16 09:02:51 +0800

    feat: 切换Menu手风琴组件

---
+++ src/layout/components/toggle-accordion/index.ts
@@ -0,0 +1,1 @@
+export { default as ToggleAccordion } from './toggle-accordion.vue';
---
+++ src/layout/components/toggle-accordion/toggle-accordion.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+defineOptions({
+  name: 'ToggleAccordion',
+});
+
+const settingsStore = useSettingsStore();
+
+/** 是否选中按钮 */
+const isActiveBtn = computed(() => {
+  return settingsStore.appSettings.layout.isMenuAccordion;
+});
+</script>
+
+<template>
+  <ActionButton
+    icon="line-md:chevron-small-double-up"
+    :is-active="isActiveBtn"
+    tips-content="是否只保持一个子菜单的展开"
+    tips-placement="right"
+    @click="settingsStore.toggleMenuAccordion"
+  />
+</template>
+
+<style scoped lang="scss"></style>
