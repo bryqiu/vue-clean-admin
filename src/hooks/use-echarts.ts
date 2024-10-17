242c426edadfe290527ef0bda518504679b810f5
fifteen <qfifteen@163.com>
2024-10-17 23:16:21 +0800

    perf(use-echarts): 优化Echarts Hook

--- src/hooks/use-echarts.ts
+++ src/hooks/use-echarts.ts
@@ -1,15 +1,56 @@
-import { type Ref, nextTick, onMounted, onUnmounted, ref, watch } from 'vue';
+import {
+  type Ref,
+  nextTick,
+  onActivated,
+  onBeforeUnmount,
+  onDeactivated,
+  onMounted,
+  ref,
+  watch,
+} from 'vue';
 import { echarts } from '@/plugins/echarts';
 import type { EChartsInitOpts } from 'echarts/core';
 import { useDebounceFn, useResizeObserver } from '@vueuse/core';
+import { ThemeModeEnum } from '@/enums';
+
+interface configOptions {
+  EchartsInitOpts?: EChartsInitOpts;
+  /**
+   * 主题模式
+   * @default light
+   */
+  theme?: ThemeModeEnum;
+  /**
+   * 是否自动调整大小
+   * @default true
+   */
+  autoResize?: boolean;
+  /**
+   * 防抖时间
+   * @default 150
+   */
+  resizeDebounceWait?: number;
+}
+
+/** 默认配置 */
+const defaultConfig: configOptions = {
+  theme: ThemeModeEnum.LIGHT,
+  autoResize: true,
+  resizeDebounceWait: 150,
+};
 
 export const useEcharts = (
   el: Ref<NullType<HTMLDivElement>>,
   options: echarts.EChartsCoreOption,
-  ECoptions?: EChartsInitOpts,
+  config: configOptions = defaultConfig,
 ) => {
-  /** 图表实例*/
+  const { EchartsInitOpts, autoResize, resizeDebounceWait } = config;
+
+  /**
+   *  图表实例
+   */
   let chartInstance: NullType<echarts.ECharts> = null;
+
   const chartOptions = ref(options);
 
   /**
@@ -17,7 +58,7 @@ export const useEcharts = (
    */
   const initChart = () => {
     if (!el.value) return;
-    chartInstance = echarts.init(el.value, null, ECoptions);
+    chartInstance = echarts.init(el.value, null, EchartsInitOpts);
 
     chartInstance.setOption(chartOptions.value);
   };
@@ -32,7 +73,7 @@ export const useEcharts = (
         duration: 300,
       },
     });
-  }, 150);
+  }, resizeDebounceWait);
 
   /**
    * 更新图表配置项
@@ -46,35 +87,50 @@ export const useEcharts = (
   };
 
   // 监听配置项的更改
-  // watch(
-  //   chartOptions,
-  //   async (newValue) => {
-  //     console.log(newValue, 'newValue');
-
-  //     updateOptions(newValue);
-  //   },
-  //   {
-  //     deep: true, // 深度监听，一般options多层级
-  //   },
-  // );
+  watch(
+    () => chartOptions.value,
+    (newValue) => {
+      updateOptions(newValue);
+    },
+    {
+      deep: true,
+    },
+  );
 
   /**
    * 销毁图表实例
    */
   const destroy = () => {
+    if (!chartInstance) return;
+
     chartInstance?.dispose();
     chartInstance = null;
   };
 
+  /**
+   * 获取图表实例
+   */
+  const getInstance = () => chartInstance;
+
   onMounted(async () => {
     await nextTick();
     initChart();
-    useResizeObserver(el, resizeChart);
+
+    autoResize && useResizeObserver(el, resizeChart);
   });
 
-  onUnmounted(() => {
+  onActivated(() => {
+    initChart();
+    resizeChart();
+  });
+
+  onDeactivated(() => {
     destroy();
   });
 
-  return { updateOptions };
+  onBeforeUnmount(() => {
+    destroy();
+  });
+
+  return { getInstance, updateOptions, destroy };
 };
