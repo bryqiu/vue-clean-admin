a622b8c0004ae601f57c62255c89902170edaa67
fifteen <qfifteen@163.com>
2024-10-31 23:12:59 +0800

    perf(Transition): update Transition Name

--- src/layout/modules/basic-main.vue
+++ src/layout/modules/basic-main.vue
@@ -1,13 +1,11 @@
-<script setup lang="ts">
-import { TransitionEnum } from '@/enums/index';
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div
     class="h-full overflow-auto overflow-x-hidden flex-1 bg-[var(--el-fill-color-light)] p-4 rounded-[var(--app-round-base)]"
   >
     <RouterView v-slot="{ Component, route }">
-      <Transition :name="TransitionEnum.SLIDE_DOWN" mode="out-in" appear>
+      <Transition name="route-fade" mode="out-in" appear>
         <KeepAlive>
           <component :is="Component" :key="route.path" />
         </KeepAlive>
