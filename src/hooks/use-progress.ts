fcd6b462cf19e366230b9d82dc1211fe6d28e8b6
fifteen <qfifteen@163.com>
2024-11-18 14:23:51 +0800

    perf(progress): 优化 Progress 配置项

--- src/hooks/use-progress.ts
+++ src/hooks/use-progress.ts
@@ -5,7 +5,7 @@ import type { NProgressOptions } from 'nprogress';
  * 默认配置
  * @see https://github.com/rstacruz/nprogress?tab=readme-ov-file#configuration
  */
-const defaultConfig: Partial<NProgressOptions> = {
+const DEFAULT_CONFIG: Partial<NProgressOptions> = {
   easing: 'ease', // 缓动动画，@see：https://developer.mozilla.org/en-US/docs/Web/CSS/transition-timing-function
   speed: 500, // 速度(ms)
   showSpinner: true, // 显示右上角旋转Icon，默认true
@@ -13,8 +13,8 @@ const defaultConfig: Partial<NProgressOptions> = {
   minimum: 0.3, // 更改启动时使用的最小百分比，默认0.08
   parent: 'body', // 父容器，默认body
 };
-export const useProgress = (config: Partial<NProgressOptions> = defaultConfig) => {
-  NProgress.configure({ ...config });
+export const useProgress = (config?: Partial<NProgressOptions>) => {
+  NProgress.configure({ ...DEFAULT_CONFIG, ...config });
 
   const start = () => {
     NProgress.start();
