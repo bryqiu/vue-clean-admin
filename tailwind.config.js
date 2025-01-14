5d98ca2a2c7384316d4200bca99a9796f6f9c400
fifteen <qfifteen@163.com>
2025-01-14 10:10:06 +0800

    feat(tailwind): 生成并配置tailwind自定义主题

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 import { addDynamicIconSelectors } from '@iconify/tailwind';
+import { twThemeConfig } from './src/config/tw-theme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './docs/**/*.{vue,js,ts,jsx,tsx}'],
   mode: 'jit',
   theme: {
-    extend: {
-      fontSize: {},
-    },
+    ...twThemeConfig,
   },
   plugins: [
     // 支持iconify图标库的图标使用
