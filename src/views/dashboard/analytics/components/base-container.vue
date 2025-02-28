6d8a942fdd18b6f382262f9ab3181a04cdd783ea
fifteen <qfifteen@163.com>
2025-02-28 15:35:30 +0800

    feat(component): 添加BaseContainer组件

---
+++ src/views/dashboard/analytics/components/base-container.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ElCard } from 'element-plus';
+
+defineOptions({
+  name: 'BaseContainer',
+});
+
+export interface BaseContainerProps {
+  title?: string;
+  containerClass?: string;
+  headerClass?: string;
+}
+
+withDefaults(defineProps<BaseContainerProps>(), {
+  title: '--',
+});
+</script>
+
+<template>
+  <ElCard shadow="never" v-bind="$attrs" class="!border-transparent shadow">
+    <div class="flex flex-col h-full gap-y-2" :class="containerClass">
+      <!--头部-->
+      <div class="w-full" :class="headerClass">
+        <template v-if="$slots.header">
+          <slot name="header" />
+        </template>
+        <template v-else>
+          <span class="text-base font-medium">{{ title }}</span>
+        </template>
+      </div>
+      <!--内容-->
+      <slot />
+    </div>
+  </ElCard>
+</template>
+
+<style scoped lang="scss"></style>
--- src/views/dashboard/analytics/components/index.ts
+++ src/views/dashboard/analytics/components/index.ts
@@ -1,3 +1,3 @@
 export { default as StatsCard, type StatsCardProps } from './stats-card.vue';
 
-export type { BoardProps } from './board-item.vue';
+export { default as BaseContainer, type BaseContainerProps } from './base-container.vue';
