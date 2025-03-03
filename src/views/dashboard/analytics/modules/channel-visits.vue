8b97cf05b55c67235b507ea4db98545c7dff8930
fifteen <qfifteen@163.com>
2025-03-03 09:55:59 +0800

    feat(component): 新增ChannelVisits(用户访问渠道分析)组件

---
+++ src/views/dashboard/analytics/modules/channel-visits.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { channelOption } from '../data';
+import { BaseContainer } from '../components';
+
+defineOptions({
+  name: 'ChannelVisits',
+});
+
+const channelInstance = ref<NullType<HTMLDivElement>>(null);
+const { renderChart } = useEcharts(channelInstance);
+
+onMounted(() => {
+  renderChart(channelOption);
+});
+</script>
+
+<template>
+  <BaseContainer title="用户访问渠道分析(月份)">
+    <!--用户访问渠道数据分析-堆叠柱状图-->
+    <div ref="channelInstance" class="size-full h-96" />
+  </BaseContainer>
+</template>
+
+<style scoped lang="scss"></style>
--- src/views/dashboard/analytics/modules/index.ts
+++ src/views/dashboard/analytics/modules/index.ts
@@ -1,1 +1,2 @@
+export { default as ChannelVisits } from './channel-visits.vue';
 export { default as AccessLogs, type AccessLog } from './access-logs.vue';
