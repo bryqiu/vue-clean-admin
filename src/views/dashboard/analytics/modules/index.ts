5519ac2494cac8ba2e57724f9e281fad7cbb2b9a
fifteen <qfifteen@163.com>
2025-03-07 14:44:38 +0800

    feat(component): 新增DataOverview(数据总览)组件

--- src/views/dashboard/analytics/modules/index.ts
+++ src/views/dashboard/analytics/modules/index.ts
@@ -1,4 +1,5 @@
 export { default as ChannelVisits } from './channel-visits.vue';
 export { default as AccessLogs, type AccessLog } from './access-logs.vue';
+export { default as DataOverview } from './data-overview.vue';
 export { default as BulletinBoard, type Bulletin } from './bulletin-board.vue';
 export { default as QuickNav, type QuickNavItem } from './quick-nav.vue';
