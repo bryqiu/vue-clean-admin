7b3f9d2659f159c48c82a325083fda6a62b66db1
fifteen <qfifteen@163.com>
2025-03-10 09:26:47 +0800

    feat(component): 新增DayVisitsTrend(日访问量折线图)组件

---
+++ src/views/dashboard/analytics/modules/day-visits-trend.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { BaseContainer } from '../components';
+import { trendOption } from '../data';
+
+defineOptions({
+  name: 'DayVisitsTrend',
+});
+
+const trendInstance = ref<NullType<HTMLDivElement>>(null);
+const { renderChart } = useEcharts(trendInstance);
+
+onMounted(() => {
+  renderChart(trendOption);
+});
+</script>
+
+<template>
+  <BaseContainer title="用户日访问量分析">
+    <div ref="trendInstance" class="w-full h-72 xl:h-96" />
+  </BaseContainer>
+</template>
+
+<style scoped lang="scss"></style>
--- src/views/dashboard/analytics/modules/index.ts
+++ src/views/dashboard/analytics/modules/index.ts
@@ -1,5 +1,6 @@
 export { default as ChannelVisits } from './channel-visits.vue';
 export { default as AccessLogs, type AccessLog } from './access-logs.vue';
 export { default as DataOverview } from './data-overview.vue';
+export { default as DayVisitsTrend } from './day-visits-trend.vue';
 export { default as BulletinBoard, type Bulletin } from './bulletin-board.vue';
 export { default as QuickNav, type QuickNavItem } from './quick-nav.vue';
