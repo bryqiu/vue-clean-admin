84bd910a4f146328c884db2b7948afde977a4781
fifteen <qfifteen@163.com>
2025-04-06 21:49:45 +0800

    chore: 删除docs目录范围

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import { twThemeConfig } from './src/config/tw-theme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './docs/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   mode: 'jit',
   darkMode: 'class',
   theme: {
