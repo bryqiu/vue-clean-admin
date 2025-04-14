8d520c1e9ad7ce45a0f619102623573470d2f714
fifteen <qfifteen@163.com>
2025-04-14 10:50:10 +0800

    feat(settings): 默认settings配置文件

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -1,28 +1,23 @@
-import type {
-  LayoutSettingsType,
-  SettingsType,
-  SundriesSettingsType,
-  ThemeSettingsType,
-} from '#/settings';
+import type { Layout, Settings, Share, Theme } from '#/settings';
 import { PageTransitionEnum, ThemeModeEnum, VisualModeEnum } from '@/enums/index';
 
 /** 默认主题设置 */
-const DEFAULT_THEME_SETTINGS: ThemeSettingsType = {
+const DEFAULT_THEME_SETTINGS: Theme = {
   currentThemeMode: ThemeModeEnum.LIGHT, // 主题模式
   primaryColor: '#25B0F0', // 主色调
   pageTransitionName: PageTransitionEnum.FADE_DOWN, // 路由切换动画
 };
 
 /** 默认布局设置 */
-const DEFAULT_LAYOUT_SETTINGS: LayoutSettingsType = {
+const DEFAULT_LAYOUT_SETTINGS: Layout = {
   isMenuCollapse: false, // 控制菜单是否折叠
   sidebarOpenedWidth: 208, // 左侧边栏展开时的宽度
   sidebarCollapseWidth: 64, // 左侧边栏收缩时的宽度
   isMenuAccordion: false, // 控制菜单是否使用手风琴模式
 };
 
-/** 默认其他设置 */
-const DEFAULT_SUNDRIES_SETTINGS: SundriesSettingsType = {
+/** 默认共享设置 */
+const DEFAULT_SHARE_SETTINGS: Share = {
   hasWatermark: false, // 是否有水印
   showBreadcrumb: true, // 是否展示面包屑
   showBreadcrumbIcon: true, // 是否显示面包屑图标
@@ -31,10 +26,10 @@ const DEFAULT_SUNDRIES_SETTINGS: SundriesSettingsType = {
 };
 
 /** 总体默认配置 */
-const DEFAULT_SETTINGS: SettingsType = {
+const DEFAULT_SETTINGS: Settings = {
   theme: { ...DEFAULT_THEME_SETTINGS },
   layout: { ...DEFAULT_LAYOUT_SETTINGS },
-  sundries: { ...DEFAULT_SUNDRIES_SETTINGS },
+  share: { ...DEFAULT_SHARE_SETTINGS },
 };
 
 export { DEFAULT_SETTINGS };
