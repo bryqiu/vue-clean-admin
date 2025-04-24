9234e41fef7deb84e7456ea66572ee0b116de4c4
fifteen <qfifteen@163.com>
2025-04-24 16:55:54 +0800

    refactor(router): 更新路由配置，重构路由导入和初始化逻辑

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,41 @@
-import { extractRoutes, generateSortRouter } from '@/utils/index';
-import { SortModeEnum } from '@/enums/index';
+import { extractRoutes, generateSortRoutes } from './helpers';
+import type { ImportGlobRoutes } from './typing';
+import { SortModeEnum } from '@/enums';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import type { RouteRecordRaw } from 'vue-router';
+import type { App } from 'vue';
 
 /** 基础路由 */
-const basicRoutes: Record<string, any> = import.meta.glob(['./modules/basic/**/*.ts'], {
+const basicRoutes = import.meta.glob<ImportGlobRoutes>(['./modules/basic/**/*.ts'], {
   eager: true,
 });
 
 /** 动态路由 */
-const dynamicRoutes: Record<string, any> = import.meta.glob(['./modules/dynamic/**/*.ts'], {
+const dynamicRoutes = import.meta.glob<ImportGlobRoutes>(['./modules/business/**/*.ts'], {
   eager: true,
 });
 
-const globalRoutes: Record<string, any> = import.meta.glob(['./modules/global/**/*.ts'], {
+const globalRoutes = import.meta.glob<ImportGlobRoutes>(['./modules/global/**/*.ts'], {
   eager: true,
 });
 
 // 菜单路由表
-export const menuRoutes = generateSortRouter(
+export const menuRoutes = generateSortRoutes(
   [...extractRoutes(basicRoutes), ...extractRoutes(dynamicRoutes)],
   SortModeEnum.ASC,
 );
 
 /** 整体路由表 */
 export const routes = [...extractRoutes(globalRoutes), ...menuRoutes];
+
+export const router = createRouter({
+  history: createWebHashHistory(),
+  routes: routes as RouteRecordRaw[],
+  strict: true,
+  scrollBehavior: () => ({ left: 0, top: 0 }),
+});
+
+/** 初始化路由 */
+export function initRouter(app: App<Element>) {
+  app.use(router);
+}
