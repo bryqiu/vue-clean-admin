f4ee22cc1d58892de84c06c3d00bbd9c6bbdf05a
fifteen <qfifteen@163.com>
2025-04-29 15:10:35 +0800

    feat(setting): 在App入口组件初始化setting配置

--- src/app.vue
+++ src/app.vue
@@ -1,4 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+const { getThemeSettings, setVisualMode, toggleThemeMode, setPrimaryColor } = useSettingsStore();
+
+const initThemeConfig = () => {
+  setVisualMode(getThemeSettings.visualMode);
+  toggleThemeMode(getThemeSettings.currentThemeMode);
+  setPrimaryColor(getThemeSettings.primaryColor);
+};
+
+onMounted(() => {
+  initThemeConfig();
+});
+</script>
 
 <template>
   <RouterView />
