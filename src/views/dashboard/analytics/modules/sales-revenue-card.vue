348f57c19fb9d032832ec1b1cebe2ec1a9977177
fifteen <qfifteen@163.com>
2025-05-30 13:51:34 +0800

    feat(card): 添加销售额卡片组件，展示总销售额及相关指标

---
+++ src/views/dashboard/analytics/modules/sales-revenue-card.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { MetricsCard, type MetricsCardProps } from '../components';
+import { ref } from 'vue';
+
+defineOptions({
+  name: 'SalesRevenueCard',
+});
+
+const salesRevenueProps = ref<MetricsCardProps>({
+  title: '总销售额(万元)',
+  numValue: '¥322,323',
+  trend: 'up',
+  compareLabel: '月度环比',
+  compareValue: '+18.6',
+  icon: 'ri:line-chart-fill',
+});
+</script>
+
+<template>
+  <MetricsCard v-bind="salesRevenueProps" />
+</template>
+
+<style scoped lang="scss"></style>
