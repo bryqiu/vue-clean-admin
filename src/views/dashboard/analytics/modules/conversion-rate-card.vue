133407e25ef91683c947265b770dacf0879611a4
fifteen <qfifteen@163.com>
2025-05-30 11:46:05 +0800

    feat(card): 添加访购转化率卡片组件，展示访购转化率及相关指标

---
+++ src/views/dashboard/analytics/modules/conversion-rate-card.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { MetricsCard, type MetricsCardProps } from '../components';
+import { ref } from 'vue';
+
+defineOptions({
+  name: 'ConversionRateCard',
+});
+
+const conversionRateProps = ref<MetricsCardProps>({
+  title: '访购转化率(%)',
+  numValue: '5.68%',
+  trend: 'up',
+  compareLabel: '月度同比',
+  compareValue: '+0.8',
+  icon: 'ri:exchange-funds-fill',
+});
+</script>
+
+<template>
+  <MetricsCard v-bind="conversionRateProps" />
+</template>
+
+<style scoped lang="scss"></style>
--- src/views/dashboard/analytics/modules/index.ts
+++ src/views/dashboard/analytics/modules/index.ts
@@ -1,6 +1,1 @@
-export { default as ChannelChart } from './channel-chart.vue';
-export { default as AccessLogs, type AccessLog } from './access-logs.vue';
-export { default as DataOverview } from './data-overview.vue';
-export { default as VisitsChart } from './visits-chart.vue';
-export { default as BulletinBoard, type Bulletin } from './bulletin-board.vue';
-export { default as QuickNav, type QuickNavItem } from './quick-nav.vue';
+export { default as ConversionRateCard } from './conversion-rate-card.vue';
