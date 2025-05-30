9d0d181a2bbd8696705a3913bf49fc3698e7c748
fifteen <qfifteen@163.com>
2025-05-30 11:49:39 +0800

    feat(card): 添加产品销售卡片组件，展示商品总销量及相关指标

--- src/views/dashboard/analytics/modules/index.ts
+++ src/views/dashboard/analytics/modules/index.ts
@@ -1,3 +1,4 @@
+export { default as ProductSalesCard } from './product-sales-card.vue';
 export { default as ConversionRateCard } from './conversion-rate-card.vue';
 export { default as ProfitMarginCard } from './profit-margin-card.vue';
 export { default as CustomerCard } from './customer-card.vue';
---
+++ src/views/dashboard/analytics/modules/product-sales-card.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { MetricsCard, type MetricsCardProps } from '../components';
+import { ref } from 'vue';
+
+defineOptions({
+  name: 'ProductSalesCard',
+});
+
+const productSalesProps = ref<MetricsCardProps>({
+  title: '商品总销量 (件)',
+  numValue: '268,432',
+  trend: 'up',
+  compareLabel: '同比上月',
+  compareValue: '15.6',
+  icon: 'ri:shopping-cart-fill',
+});
+</script>
+
+<template>
+  <MetricsCard v-bind="productSalesProps" />
+</template>
+
+<style scoped lang="scss"></style>
