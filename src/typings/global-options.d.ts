8e6eda7e048daea1164d0a510166b63e079d33f0
fifteen <qfifteen@163.com>
2025-06-04 17:16:07 +0800

    refactor(types): 将全局类型定义移动到global.d.ts

--- src/typings/global-options.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-  type AlignMode = 'horizontal' | 'vertical';
-  type BreadcrumbStyleType = 'default' | 'arrow' | 'parallelogram';
-
-  type ElColorType = 'primary' | 'success' | 'warning' | 'danger' | 'info';
-  type ColorWeight = [100, 200, 300, 400, 500, 600, 700, 800, 900];
-}
-
-export {};
--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -2,6 +2,12 @@ import { PropType as propType } from 'vue';
 import type { Component, DefineComponent } from 'vue';
 
 declare global {
+  type AlignMode = 'horizontal' | 'vertical';
+  type BreadcrumbStyleType = 'default' | 'arrow' | 'parallelogram';
+
+  type ElColorType = 'primary' | 'success' | 'warning' | 'danger' | 'info';
+  type ColorWeight = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+
   type NullType<T> = T | null;
 
   type PropType<T> = propType<T>;
