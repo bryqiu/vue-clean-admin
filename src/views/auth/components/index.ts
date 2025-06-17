3101906e57a313e509254eceab6d8b800b8ddb20
fifteen <qfifteen@163.com>
2025-06-17 22:38:47 +0800

    feat(component): 添加pattern-background组件，用于渲染背景网格

--- src/views/auth/components/index.ts
+++ src/views/auth/components/index.ts
@@ -1,4 +1,5 @@
 export { default as PageHeader } from './page-header.vue';
 export { default as Particles } from './particles.vue';
+export { default as PatternBackground } from './pattern-background.vue';
 
 export type { PageHeaderProps } from './page-header.vue';
---
+++ src/views/auth/components/pattern-background.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    :class="
+      twMerge(
+        'relative text-clip bg-[linear-gradient(to_right,var(--el-border-color-light)_1px,transparent_1px),linear-gradient(to_bottom,var(--el-border-color-light)_1px,transparent_1px)] bg-[size:24px_24px]',
+        'flex size-full justify-center items-center',
+      )
+    "
+  >
+    <div
+      :class="
+        twMerge(
+          'absolute pointer-events-none inset-0',
+          'bg-el-overlay',
+          '[mask-image:radial-gradient(ellipse_at_center,transparent,black_80%)]',
+        )
+      "
+    />
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { twMerge } from 'tailwind-merge';
+</script>
+
+<style scoped></style>
