1c737687d8355584a264067bb6005889297c3b65
fifteen <qfifteen@163.com>
2025-06-19 13:53:07 +0800

    chore(auth): 删除未使用的particles组件

--- src/views/auth/components/index.ts
+++ src/views/auth/components/index.ts
@@ -1,5 +1,4 @@
 export { default as PageHeader } from './page-header.vue';
-export { default as Particles } from './particles.vue';
 export { default as PatternBackground } from './pattern-background.vue';
 
 export type { PageHeaderProps } from './page-header.vue';
