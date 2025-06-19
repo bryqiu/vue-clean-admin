fbdee981a6b88adcaee9e88eb1a811864557e572
fifteen <qfifteen@163.com>
2025-06-19 13:54:06 +0800

    refactor(auth): 调整介绍组件、登录组件及优化布局

--- src/views/auth/auth-layout.vue
+++ src/views/auth/auth-layout.vue
@@ -8,13 +8,13 @@ defineOptions({
 
 <template>
   <div class="size-full grid grid-cols-5 gap-2">
-    <!--左侧登录-->
-    <div class="col-span-5 lg:col-span-2">
-      <Account />
-    </div>
-    <!--右侧介绍-->
+    <!--左侧介绍-->
     <div class="col-span-3 hidden lg:block">
       <Introduce />
+    </div>
+    <!--右侧登录-->
+    <div class="col-span-5 lg:col-span-2">
+      <Account />
     </div>
   </div>
 </template>
