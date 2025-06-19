f44357f147aa46d18636888a39243354b346d50e
fifteen <qfifteen@163.com>
2025-06-19 13:52:45 +0800

    feat(auth): 美化登录左侧介绍页面

--- src/views/auth/modules/introduce.vue
+++ src/views/auth/modules/introduce.vue
@@ -1,54 +1,104 @@
 <script setup lang="ts">
-import { Particles } from '../components';
+import { PatternBackground } from '../components';
+import { LocalIconProps } from '@/components/common/app-icon';
+import { Logo } from '@/components/common/logo';
 
 defineOptions({
   name: 'Introduce',
 });
 
-const featureConfig = [
+interface TechStack {
+  iconProps: LocalIconProps;
+  title: string;
+}
+
+interface LinkConfig {
+  iconName: string;
+  title?: string;
+  url: string;
+}
+
+const techStacks: TechStack[] = [
   {
-    icon: 'ri:flashlight-fill',
-    title: '快速开发',
-    desc: '基于 Vue3 + Vite 技术栈，提供完整的开发解决方案',
+    iconProps: {
+      name: 'vue',
+    },
+    title: 'Vue3',
   },
   {
-    icon: 'ri:flashlight-fill',
-    title: '快速开发',
-    desc: '基于 Vue3 + Vite 技术栈，提供完整的开发解决方案',
+    iconProps: {
+      name: 'vite',
+    },
+    title: 'Vite',
+  },
+  {
+    iconProps: {
+      name: 'type-script',
+    },
+    title: 'TypeScript',
+  },
+  {
+    iconProps: {
+      name: 'element-plus',
+      color: '#409eff',
+    },
+    title: 'Element Plus',
   },
 ];
+
+const linkConfig: LinkConfig[] = [
+  {
+    title: '查看相关文档',
+    iconName: 'vite-press',
+    url: '',
+  },
+  {
+    iconName: 'git-hub',
+    url: 'https://github.com/bryqiu/vue-clean-admin',
+  },
+];
+
+const handleLinkClick = (url: string) => {
+  if (!url) return;
+  window.open(url, '_blank');
+};
 </script>
 
 <template>
-  <div class="relative size-full flex flex-col p-6 bg-el-lighter">
-    <Particles
-      class="absolute inset-0"
-      :quantity="200"
-      :ease="100"
-      :color="'#000'"
-      :staticity="10"
-      refresh
-    />
-    <div class="flex-1 flex flex-col items-center justify-center gap-y-2">
-      <span
-        class="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-5xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10"
-      >
-        Vue Clean Admin
-      </span>
+  <div class="size-full border border-solid border-el-lighter relative">
+    <div class="absolute top-4 left-4 z-10">
+      <Logo :is-collapse="true" />
     </div>
-
-    <div class="w-full grid grid-cols-2">
-      <div
-        v-for="(feature, index) in featureConfig"
-        :key="index"
-        class="flex flex-col gap-y-4 border border-solid border-el-dark justify-center items-center py-8 px-6"
-        :class="[index % 2 !== 0 ? 'border-l-0 rounded-r-lg' : 'rounded-l-lg']"
-      >
-        <AppIcon :icon="feature.icon" class="text-6xl text-el-secondary" />
-        <span class="text-xl font-semibold">{{ feature.title }}</span>
-        <span class="text-sm text-center">{{ feature.desc }}</span>
+    <PatternBackground>
+      <div class="flex flex-col items-center">
+        <span class="mb-3 text-center text-4xl font-bold md:text-4xl text-el-primary"
+          >Vue Clean Admin</span
+        >
+        <div class="text-base text-el-secondary flex flex-col items-center">
+          <span>一套基于 Vue3 + Vite + TypeScript + Element Plus </span>
+          <span>构建的企业级中后台模板，提供高效、可靠的中后台解决方案</span>
+        </div>
+        <div class="flex items-center gap-2 mx-auto mt-8 mb-12">
+          <div
+            v-for="(item, index) in linkConfig"
+            :key="index"
+            class="flex h-12 w-fit items-center gap-2 rounded-lg bg-el-dark px-4 py-2 text-center cursor-pointer border border-solid border-transparent hover:opacity-60 hover:border-el-darker duration-300"
+            @click="handleLinkClick(item.url)"
+          >
+            <div class="flex items-center gap-2">
+              <LocalIcon :name="item.iconName" :size="24" />
+              <span v-if="item.title" class="text-sm">{{ item.title }}</span>
+            </div>
+          </div>
+        </div>
+        <div class="w-full flex items-center gap-4">
+          <div v-for="(tech, index) in techStacks" :key="index" class="flex items-center gap-2 p-2">
+            <LocalIcon v-bind="tech.iconProps" :size="24" />
+            <span class="text-sm font-medium">{{ tech.title }}</span>
+          </div>
+        </div>
       </div>
-    </div>
+    </PatternBackground>
   </div>
 </template>
 
