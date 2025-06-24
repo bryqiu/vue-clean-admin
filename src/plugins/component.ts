7dc7c6bb8d2ff342738438cb28e75df589af329d
fifteen <qfifteen@163.com>
2025-06-24 17:27:36 +0800

    refactor(stystem): 移动全局组件注册逻辑至插件文件

--- src/plugins/component.ts
+++ src/plugins/component.ts
@@ -1,12 +1,48 @@
 import type { App } from 'vue';
-import { globalCompList, globalPluginList } from '@/config';
+import type { Component, DefineComponent } from 'vue';
+
+import { ElButton, ElLoading } from 'element-plus';
+import { ActionButton } from '@/components/common/action-button';
+import { IconifyIcon, LocalIcon } from '@/components/common/app-icon';
+
+interface RegisterComponent {
+  name: string;
+  component: Component | DefineComponent;
+}
+
+/** 全局注册的Element Plus组件 */
+const globalElComponents: RegisterComponent[] = [
+  {
+    name: ElButton.name || 'ElButton',
+    component: ElButton,
+  },
+];
+
+/** 全局注册的项目内部组件 */
+const globalAppComponents: RegisterComponent[] = [
+  {
+    name: 'ActionButton',
+    component: ActionButton,
+  },
+  {
+    name: 'IconifyIcon',
+    component: IconifyIcon,
+  },
+  {
+    name: 'LocalIcon',
+    component: LocalIcon,
+  },
+];
+
+const globalCompList = [...globalElComponents, ...globalAppComponents];
+const globalPluginList = [ElLoading];
 
 /**
  * 注册全局组件
  * @param app Vue应用实例
  */
 const registerGlobalComp = (app: App<Element>) => {
-  if (globalCompList.length === 0) return;
+  if (!globalCompList.length) return;
   globalCompList.forEach((item) => {
     app.component(item.name, item.component);
   });
