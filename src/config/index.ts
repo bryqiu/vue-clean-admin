73680e67dcbe2306d1084e6f520c7332f08ac106
fifteen <qfifteen@163.com>
2025-06-24 17:34:19 +0800

    chore(config): 删除不再使用的配置文件

--- src/config/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './global-modules';
-export * from './global-modules';
--- src/config/router.ts
+++
@@ -1,4 +0,0 @@
-export const LAYOUT = () => import('@/layout/index.vue');
-
-/** 根路径-重定向路径 */
-export const ROOT_REDIRECT = '/dashboard';
