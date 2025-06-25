90ead837f7b51028d3ea9461fb871476994fb81d
fifteen <qfifteen@163.com>
2025-06-25 16:05:57 +0800

    feat(app-version): 添加应用版本组件，支持简写模式显示版本号

---
+++ src/components/common/app-version/app-version.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'AppVersion',
+});
+
+interface AppVersionProps {
+  /** 是否以简写模式显示版本号 */
+  abbreviated?: boolean;
+}
+
+withDefaults(defineProps<AppVersionProps>(), {
+  abbreviated: false,
+});
+
+const currentVersion = import.meta.env.VITE_APP_VERSION;
+</script>
+
+<template>
+  <div class="flex items-center justify-center text-sm text-el-placeholder gap-x-1">
+    <template v-if="abbreviated">
+      <span>v {{ currentVersion }}</span>
+    </template>
+    <template v-else>
+      <IconifyIcon name="mingcute:version-line" />
+      <span>Version</span>
+      <span>{{ currentVersion }}</span>
+    </template>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
