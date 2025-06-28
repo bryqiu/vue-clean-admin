76d37aba8b89d25f19f8d58ab26a8903a6ae6e30
fifteen <qfifteen@163.com>
2025-06-28 21:37:15 +0800

    feat(analytics): 添加筛选和导出按钮，优化布局设计

--- src/views/dashboard/analytics/analytics.vue
+++ src/views/dashboard/analytics/analytics.vue
@@ -2,22 +2,37 @@
 import {
   ConversionRateCard,
   CustomerCard,
-  MonthOrderChart,
   ProductSalesCard,
   ProductTrendChart,
   ProfitMarginCard,
   SalesRevenueCard,
 } from './modules';
+import { h } from 'vue';
+import { IconifyIcon } from '@/components/common/app-icon';
 
 defineOptions({
   name: 'Analytics',
+});
+
+const filterIcon = h(IconifyIcon, {
+  name: 'ri:filter-2-line',
+});
+
+const exportIcon = h(IconifyIcon, {
+  name: 'ri:download-2-line',
 });
 </script>
 
 <template>
   <div class="flex flex-col gap-y-4">
-    <div class="flex items-center">
-      <span class="text-el-primary text-3xl font-medium">Dashboard</span>
+    <div class="flex items-center justify-between">
+      <span class="text-el-primary text-3xl font-bold">Dashboard</span>
+      <div class="flex items-center">
+        <ElButton plain :icon="filterIcon">
+          <span>筛选</span>
+        </ElButton>
+        <ElButton plain :icon="exportIcon"> 导出数据 </ElButton>
+      </div>
     </div>
 
     <div class="w-full grid grid-cols-6 gap-4">
@@ -28,13 +43,12 @@ defineOptions({
         <ProfitMarginCard />
       </div>
       <div class="col-span-6 lg:col-span-3">
-        <MonthOrderChart />
+        <ProductTrendChart class="col-span-1" />
       </div>
     </div>
 
-    <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
-      <ProductTrendChart class="col-span-1" />
-      <CustomerCard class="col-span-1" />
+    <div>
+      <CustomerCard />
     </div>
   </div>
 </template>
