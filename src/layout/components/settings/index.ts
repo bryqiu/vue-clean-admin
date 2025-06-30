4cb8d17c9f926c2e1fd04c9966819e9cfa88fe45
fifteen <qfifteen@163.com>
2025-06-30 22:42:36 +0800

    refactor(settings): 导出名称SettingsAction重命名为Settings

--- src/layout/components/settings/index.ts
+++ src/layout/components/settings/index.ts
@@ -1,2 +1,2 @@
-export { default as SettingsAction } from './settings-action.vue';
+export { default as Settings } from './settings-action.vue';
 export { default as SettingsDrawer } from './settings-drawer.vue';
