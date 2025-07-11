929f456bc91031fdd4255ce675fa5505638e13ad
fifteen <qfifteen@163.com>
2025-07-11 22:23:54 +0800

    feat(app-version): 增加 textClass 属性以支持自定义文本样式

--- src/components/common/app-version/app-version.vue
+++ src/components/common/app-version/app-version.vue
@@ -1,22 +1,40 @@
 <script setup lang="ts">
+import { twMerge } from 'tailwind-merge';
+
 defineOptions({
   name: 'AppVersion',
 });
 
 interface AppVersionProps {
-  /** 是否以简写模式显示版本号 */
+  /**
+   * 是否以简写模式显示版本号
+   * @default false
+   */
   abbreviated?: boolean;
+  /**
+   * 文本类
+   * @default ''
+   */
+  textClass?: string;
 }
 
 withDefaults(defineProps<AppVersionProps>(), {
   abbreviated: false,
+  textClass: '',
 });
 
 const currentVersion = import.meta.env.VITE_APP_VERSION;
 </script>
 
 <template>
-  <div class="flex items-center justify-center text-sm text-el-text-placeholder gap-x-1">
+  <div
+    :class="
+      twMerge(
+        'flex items-center justify-center text-sm text-el-text-placeholder gap-x-1',
+        textClass,
+      )
+    "
+  >
     <template v-if="abbreviated">
       <span>v {{ currentVersion }}</span>
     </template>
