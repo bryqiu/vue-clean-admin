1845cbb686dca5ee321ce893c7409840fb92ae6c
fifteen <qfifteen@163.com>
2025-12-09 17:47:32 +0800

    refactor: iconify 组件属性配置

--- src/components/common/app-icon/iconify-icon.vue
+++ src/components/common/app-icon/iconify-icon.vue
@@ -1,23 +1,40 @@
 <script setup lang="ts">
 import { Icon as IconifyIcon } from '@iconify/vue';
 import type { IconProps } from '@iconify/vue';
+import { omit } from 'lodash-es';
+import { computed, useAttrs } from 'vue';
 
 defineOptions({
   name: 'IconifyIcon',
 });
 
-export interface IconifyIconProps extends Omit<IconProps, 'icon'> {
+export interface IconifyIconProps {
   /**
    * 图标名称
    */
   name: string;
 }
 
-withDefaults(defineProps<IconifyIconProps>(), {});
+const props = withDefaults(defineProps<IconifyIconProps>(), {});
+
+const attrs = useAttrs();
+
+const getIconifyIconProps = computed(() => {
+  const defaultProps: IconProps = {
+    inline: true,
+    icon: props.name,
+  };
+  const iconifyIconProps = omit(attrs, 'icon');
+
+  return {
+    ...defaultProps,
+    ...iconifyIconProps,
+  };
+});
 </script>
 
 <template>
-  <IconifyIcon :icon="name" :inline="true" v-bind="$attrs" />
+  <IconifyIcon v-bind="getIconifyIconProps" />
 </template>
 
 <style scoped lang="scss"></style>
