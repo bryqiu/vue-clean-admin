a0f024083cde10e3ced5c17a58235c69595b7931
fifteen <qfifteen@163.com>
2026-01-27 16:12:31 +0800

    types: 全局组件、全局指令的类型定义

--- src/typings/app-components.d.ts
+++ src/typings/app-components.d.ts
@@ -1,9 +1,11 @@
 export {};
-// 全局组件的类型声明，用于Vue - Official的组件高亮及类型提示
+
 declare module 'vue' {
+  // 全局组件的类型声明，用于Vue - Official的组件高亮及类型提示
   export interface GlobalComponents {
     ActionButton: (typeof import('../components/common/action-button/index'))['ActionButton'];
     IconifyIcon: (typeof import('../components/common/app-icon/index'))['IconifyIcon'];
     LocalIcon: (typeof import('../components/common/app-icon/index'))['LocalIcon'];
+    AppAuth: (typeof import('../components/common/app-auth/index'))['AppAuth'];
   }
 }
